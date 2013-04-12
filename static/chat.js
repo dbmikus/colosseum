d19104b6e6181ab8ef4886cd23036af5192cf08b
Dylan Mikus <dbmikus@gmail.com>
2013-04-11 20:05:44 -0400

    More index.html stuff

---
+++ static/chat.js
@@ -0,0 +1,12 @@
+// Stuff for spectator and competitor chat
+
+// Will be more complete later
+function specLogin(username) {
+}
+
+function userSay(user, text) {
+    var specChat = $('#chat');
+}
+
+function otherSay(user, text) {
+}
