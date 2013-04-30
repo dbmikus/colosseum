88238da586582640f10de03cfec6e740923b4d3b
Zachary Matarasso <zmatarasso@gmail.com>
2013-04-30 00:56:34 -0400

    Some small changes to styling and findarena.js

--- www-src/findarena.js
+++ www-src/findarena.js
@@ -99,14 +99,16 @@ function makeArenaItemDesktop(id, player1, player2, name, numSpectators) {
     link.attr("href", "/arena?id=" + id);
     link.append(li);
 	li.html(name);
-    var player1 = $('<div>').addClass('listPlayer1');
-	var player2 = $('<div>').addClass('listPlayer2');
-	var player1Name = $('<p>').html(player1);
+    var user1 = $('<div>').addClass('listPlayer1');
+	var user2 = $('<div>').addClass('listPlayer2');
+	var player1Name = $('<p>').html(player);
 	player1Name.addClass('listName1')
 	var player2Name	= $('<p>').html(player2);
 	player2Name.addClass('listName2')
-	player1.append(player1Name);
-	player2.append (player2Name);
+	user1.append(player1Name);
+	user2.append (player2Name);
+	li.append(user1);
+	li.append(user2);
 	var viewers = li.append($('<div>').html(numSpectators));
 	viewers.addClass('listViewers');
 	var versus = li.append($('<div>').html('vs'));
